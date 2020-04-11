eec95c733673aa6c8b28044cdab99a7ef31c1ea9
mikapersson <perssonmika@gmail.com>
2020-04-11 22:51:30 +0200

    finished base case

--- Lab4-ClosestPair/solution.py
+++ Lab4-ClosestPair/solution.py
@@ -1,5 +1,7 @@
 import sys
 from readcreatepoints import read_create_points
+from math import inf
+from point import distance, check_pair
 
 """Solution for Closest Pair lab"""
 
@@ -24,7 +26,17 @@ def closest_rec(p_x, p_y, size):
     """
     Recursive function for dividing the set of points.
     """
-    # BASE CASE, WHEN DO WE DO THIS?
+    if size <= 3:  # base case 1, compute minimum distance of the points (may be compressed)
+        base_distance = inf
+        base_points = [0]*size
+        for index, base_point in enumerate(p_x):
+            base_points[index] = base_point
+
+        for p1 in range(size-1):  # loop through base points, no need to loop until the last point
+            for p2 in range(p1, size):
+                temp_distance = distance(base_points[p1], base_points[p2])
+                if temp_distance < base_distance:
+                    base_distance = temp_distance
 
     cut = size//2  # middle index of points lists
     middle_point = p_x[cut]
@@ -44,6 +56,20 @@ def closest_rec(p_x, p_y, size):
     left_distance = closest_rec(left_x, left_y, cut)  # calculate minimum distance on left side
     right_distance = closest_rec(right_x, right_y, size - cut)  # -||- right side
     delta = min(left_distance, right_distance)
+
+    interval_points = []  # points that have an x-coordinate within the distance 'delta' of the 'middle_point'
+    left_interval = set()
+    right_interval = set()
+    for y_point in p_y:
+        inside_interval = abs(y_point.x - middle_point.x) < delta
+        if inside_interval:  # y_point is inside the middle interval of width 2*delta
+            interval_points.append(y_point)
+            if y_point in left_side:
+                left_interval.add(y_point)
+            else:
+                right_interval.add(y_point)
+
+    
 
     return 0
 
