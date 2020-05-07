e399296c1021c0f9fc9728ab08428bb26c47afc4
mikapersson <perssonmika@gmail.com>
2020-05-07 09:30:46 +0200

    call indices instead of strings

---
+++ Lab5-StringMatching/secondtry.py
@@ -0,0 +1,157 @@
+from setup import setup
+from math import inf
+
+
+"""First try of solving lab 5, dynamic programming"""
+
+
+def gain(let1, let2):
+    """
+    Calculates the gain of the two letters 'let1' and 'let2'
+
+    Parameters
+    ----------
+    let1 : str
+        First letter
+    let2 : str
+        Second Letter
+    Returns
+    -------
+    int
+        The gain of 'let1' and 'let2' according to the gain matrix
+    """
+    return int(gain_matrix[let_to_ind[let1]][let_to_ind[let2]])
+
+
+def align_words(string1, string2):
+    """
+    Find optimal alignment, given 'string1' and 'string2',
+    such that the total gain of aligning the strings in maximized
+
+    Parameters
+    ----------
+    string1 : str
+        First string in query
+    string2 : str
+        Second string in query
+    """
+    init_gain = 0
+    res1 = ''
+    res2 = ''
+    tot_gain, res1, res2 = align_words_rec(string1, string2, res1, res2, init_gain)
+
+    # result1 = ''.join(result1_list)
+    # result2 = ''.join(result2_list)
+    print(res1, res2)  # print resulting alignment
+
+
+def align_words_rec(string1, string2, result1, result2, total_gain):
+    """
+    Recursive help function for 'align_words' function (can be shortened + use help functions)
+
+    Parameters
+    ----------
+    string1 : str
+        First string as in 'align_words'
+    string2 : str
+        Second string as in 'align_words'
+    result1 : str
+        Result of the first word in the current query
+    result2 : str
+        Result of the second word in the current query
+    total_gain : int
+        Current total gain (from before, not 'string1' and 'string2'
+
+    Returns
+    -------
+    (int, string, string)
+        (total gain between 'string1' and 'string2', aligned first string, aligned second string)
+    """
+
+    # if we've already computed the value for (string1, string2, result1, result2, total_gain)
+    if (string1, string2, result1, result2) in align_cache:
+        return align_cache[(string1, string2, result1, result2)]  # BEHÖVS result1 och result2??
+    else:
+
+        length1 = len(string1)
+        length2 = len(string2)
+        letter1 = string1[-1]
+        letter2 = string2[-1]
+
+        if length1 == 1 and length2 == 1:   # base case 1
+            total_gain = gain(letter1, letter2)
+            string1_result = letter1
+            string2_result = letter2
+        elif length1 > 1 and length2 == 1:  # base case 2
+            # decide on which side of letter2 we put '*'
+            left_gain = gain(string1[1], letter2)
+            right_gain = gain(string1[0], letter2)
+
+            if left_gain > right_gain:
+                total_gain = left_gain
+                string1_result = string1
+                string2_result = '*' + string2
+            else:
+                total_gain = right_gain
+                string1_result = string1
+                string2_result = string2 + '*'
+
+        elif length1 == 1 and length2 > 1:  # base case 3
+            # decide on which side of letter2 we put '*'
+            left_gain = gain(letter1, string2[1])
+            right_gain = gain(letter1, string2[0])
+
+            if left_gain > right_gain:
+                total_gain = left_gain
+                string1_result = '*' + string1
+                string2_result = string2
+            else:
+                total_gain = right_gain
+                string1_result = string1 + '*'
+                string2_result = string2
+        else:
+
+            # no '*' added
+            gain1, res11, res12 = align_words_rec(string1[:-1], string2[:-1], result1, result2, total_gain)
+            gain1 += gain(letter1, letter2)
+
+            # '*' added after letter1
+            gain2, res21, res22 = align_words_rec(string1, string2[:-1], result1, result2, total_gain)
+            gain2 -= 4
+
+            # '*' added after letter2
+            gain3, res31, res32 = align_words_rec(string1[:-1], string2, result1, result2, total_gain)
+            gain3 -= 4
+
+            max_gain = max(gain1, gain2, gain3)
+
+            if max_gain == gain1:
+                max_res1 = res11 + letter1
+                max_res2 = res12 + letter2
+            else:
+                if max_gain == gain2:
+                    max_res1 = res21 + '*'
+                    max_res2 = res22 + letter2
+                elif max_gain == gain3:
+                    max_res1 = res31 + letter1
+                    max_res2 = res32 + '*'
+
+            string1_result = max_res1
+            string2_result = max_res2
+
+        align_cache[(string1, string2, result1, result2)] = total_gain, string1_result, string2_result
+        return total_gain, string1_result, string2_result
+
+
+gain_matrix, let_to_ind, queries = setup()  # read input, set up cost_matrix and queries
+align_cache = {}
+
+
+for query in queries:
+    word1 = query[0]
+    word2 = query[1]
+    align_words(word1, word2)
+
+
+
+
--- Lab5-StringMatching/solution.py
+++ Lab5-StringMatching/solution.py
@@ -23,7 +23,7 @@ def gain(let1, let2):
     return int(gain_matrix[let_to_ind[let1]][let_to_ind[let2]])
 
 
-def align_words(string1, string2):
+def align_words():
     """
     Find optimal alignment, given 'string1' and 'string2',
     such that the total gain of aligning the strings in maximized
@@ -35,32 +35,28 @@ def align_words(string1, string2):
     string2 : str
         Second string in query
     """
-    init_gain = 0
-    res1 = ''
-    res2 = ''
-    tot_gain, res1, res2 = align_words_rec(string1, string2, res1, res2, init_gain)
 
-    # result1 = ''.join(result1_list)
-    # result2 = ''.join(result2_list)
+    global word1
+    global word2
+
+    tot_gain, res1, res2 = align_words_rec(len(word1)-1, len(word2)-1)
+
+    # DO BACKTRACKING
+
     print(res1, res2)  # print resulting alignment
 
 
-def align_words_rec(string1, string2, result1, result2, total_gain):
+def align_words_rec(pos1, pos2):
     """
-    Recursive help function for 'align_words' function (can be shortened + use help functions)
+    Recursive help function for 'align_words' function,
+    actually fills out 'align_cache'
 
     Parameters
     ----------
-    string1 : str
-        First string as in 'align_words'
-    string2 : str
-        Second string as in 'align_words'
-    result1 : str
-        Result of the first word in the current query
-    result2 : str
-        Result of the second word in the current query
-    total_gain : int
-        Current total gain (from before, not 'string1' and 'string2'
+    int : pos1
+        Last position in string1
+    int : pos2
+        Last position in string2
 
     Returns
     -------
@@ -68,62 +64,44 @@ def align_words_rec(string1, string2, result1, result2, total_gain):
         (total gain between 'string1' and 'string2', aligned first string, aligned second string)
     """
 
-    # if we've already computed the value for (string1, string2, result1, result2, total_gain)
-    if (string1, string2, result1, result2) in align_cache:
-        return align_cache[(string1, string2, result1, result2)]  # BEHÖVS result1 och result2??
+    # if we've already computed the value for (pos1, pos2)
+    if (pos1, pos2) in align_cache:
+        return align_cache[(pos1, pos2)]
     else:
 
-        length1 = len(string1)
-        length2 = len(string2)
-        letter1 = string1[-1]
-        letter2 = string2[-1]
+        letter1 = word1[pos1]
+        letter2 = word2[pos2]
 
-        if length1 == 1 and length2 == 1:   # base case 1
-            total_gain = 0
-            string1_result = letter1
-            string2_result = letter2
-        elif length1 > 1 and length2 == 1:  # base case 2
-            total_gain = -4 * length1
+        if pos1 == 1 and pos2 == 1:   # base case 1
+            max_gain = gain(letter1, letter2)
+        elif pos1 > 1 and pos2 == 1:  # base case 2
+            left_gain = gain(word1[1], letter2)
+            right_gain = gain(word1[0], letter2)
 
-            string1_result = string1
-            string2_result = '*' * length1
-        elif length1 == 1 and length2 > 1:  # base case 3
-            total_gain = -4 * length2
+            max_gain = max(left_gain, right_gain)
 
-            string1_result = '*' * length2
-            string2_result = string2
+        elif pos1 == 1 and pos2 > 1:  # base case 3
+            left_gain = gain(letter1, word2[1])
+            right_gain = gain(letter1, word2[0])
+
+            max_gain = max(left_gain, right_gain)
         else:
 
             # no '*' added
-            gain1, res11, res12 = align_words_rec(string1[:-1], string2[:-1], result1, result2, total_gain)
+            gain1, res11, res12 = align_words_rec(pos1-1, pos2-1)
             gain1 += gain(letter1, letter2)
 
             # '*' added after letter1
-            gain2, res21, res22 = align_words_rec(string1, string2[:-1], result1, result2, total_gain)
+            gain2, res21, res22 = align_words_rec(pos1, pos2-1)
             gain2 -= 4
 
             # '*' added after letter2
-            gain3, res31, res32 = align_words_rec(string1[:-1], string2, result1, result2, total_gain)
+            gain3, res31, res32 = align_words_rec(pos1-1, pos2)
             gain3 -= 4
 
             max_gain = max(gain1, gain2, gain3)
 
-            if max_gain == gain1:
-                max_res1 = res11 + letter1
-                max_res2 = res12 + letter2
-            else:
-                if max_gain == gain2:
-                    max_res1 = res21 + '*'
-                    max_res2 = res22 + letter2
-                elif max_gain == gain3:
-                    max_res1 = res31 + letter1
-                    max_res2 = res32 + '*'
-
-            string1_result = max_res1
-            string2_result = max_res2
-
-        align_cache[(string1, string2, result1, result2)] = total_gain, string1_result, string2_result
-        return total_gain, string1_result, string2_result
+        align_cache[(pos1, pos2)] = max_gain
 
 
 gain_matrix, let_to_ind, queries = setup()  # read input, set up cost_matrix and queries
@@ -133,7 +111,8 @@ align_cache = {}
 for query in queries:
     word1 = query[0]
     word2 = query[1]
-    align_words(word1, word2)
+    align_words()
+    align_cache.clear()
 
 
 
