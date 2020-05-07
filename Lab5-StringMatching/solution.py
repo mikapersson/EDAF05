eee0e84a3607a33929f7bc75b898169d9b08d9e6
mikapersson <perssonmika@gmail.com>
2020-05-07 10:46:37 +0200

    debugging

--- Lab5-StringMatching/solution.py
+++ Lab5-StringMatching/solution.py
@@ -23,7 +23,7 @@ def gain(let1, let2):
     return int(gain_matrix[let_to_ind[let1]][let_to_ind[let2]])
 
 
-def backtrack():
+def backtrack():  # COMPLEXITY?
     """
     Finds the optimal alignment of 'word1' and 'word2' with respect to 'align_cache'.
 
@@ -35,18 +35,36 @@ def backtrack():
         'word2' aligned
     """
 
+    aligned1 = ""
+    aligned2 = ""
+
+    pos1 = len(word1)-1
+    pos2 = len(word2)-1
+
+    while pos1 >= 0 and pos2 >= 0:
+        letter1 = word1[pos1]
+        letter2 = word2[pos2]
+        ignore, case = align_cache([pos1, pos2])
+
+        if case == 0:
+            aligned1 = letter1 + aligned1
+            aligned2 = letter2 + aligned2
+            pos1 -= 1
+            pos2 -= 1
+        elif case == 1:
+            aligned1 = '*' + aligned1
+            pos2 -= 1
+        else:
+            aligned2 = letter2 + aligned2
+            pos1 -= 1
+
+    return aligned1, aligned2
+
 
 def align_words():
     """
     Find optimal alignment, given 'string1' and 'string2',
     such that the total gain of aligning the strings in maximized
-
-    Parameters
-    ----------
-    string1 : str
-        First string in query
-    string2 : str
-        Second string in query
     """
 
     global word1
@@ -57,17 +75,14 @@ def align_words():
     print(aligned1, aligned2)
 
 
-def align_words_rec(pos1, pos2):
+def align_words_rec(pos1, pos2):  # COMPLEXITY?
     """
     Recursive help function for 'align_words' function,
     actually fills out 'align_cache'
 
-    Parameters
-    ----------
-    int : pos1
-        Last position in string1
-    int : pos2
-        Last position in string2
+    :param:
+        int : pos1, current index in 'word1'
+        int : pos2, current index in 'word2'
 
     Returns
     -------
@@ -79,36 +94,45 @@ def align_words_rec(pos1, pos2):
     if (pos1, pos2) in align_cache:
         return align_cache[(pos1, pos2)]
     else:
-        letter1 = word1[pos1]
-        letter2 = word2[pos2]
+        position = None
 
-        if pos1 == 0 and pos2 == 0:   # base case 1
-            max_gain = gain(letter1, letter2)
-        elif pos1 > 0 and pos2 == 0:  # base case 2
-            temp_gain = align_words_rec(pos1-1, pos2)
-            max_gain = temp_gain - 4
-        elif pos1 == 0 and pos2 > 0:  # base case 3
-            temp_gain = align_words_rec(pos1, pos2-1)
-            max_gain = temp_gain - 4
+        if pos1 == -1 and pos2 == -1:
+            position = 0
+            max_gain = 0
+        elif pos1 > -1 and pos2 == -1:  # base case 2
+            position = 1
+            max_gain = -4 * pos1
+        elif pos1 == -1 and pos2 > -1:  # base case 3
+            position = 2
+            max_gain = -4 * pos2
         else:
 
+            letter1 = word1[pos1]
+            letter2 = word2[pos2]
 
             # no '*' added
-            gain1 = align_words_rec(pos1-1, pos2-1)
+            gain1, position0 = align_words_rec(pos1-1, pos2-1)
             gain1 += gain(letter1, letter2)
 
             # '*' added after letter1
-            gain2 = align_words_rec(pos1, pos2-1)
+            gain2, position1 = align_words_rec(pos1, pos2-1)
             gain2 -= 4
 
             # '*' added after letter2
-            gain3 = align_words_rec(pos1-1, pos2)
+            gain3, position2 = align_words_rec(pos1-1, pos2)
             gain3 -= 4
 
             max_gain = max(gain1, gain2, gain3)
 
-        align_cache[(pos1, pos2)] = max_gain
-        return max_gain
+            if max_gain == gain1:
+                position = position0
+            elif max_gain == gain2:
+                position = position1
+            else:
+                position = position2
+
+        align_cache[(pos1, pos2)] = max_gain, position
+        return max_gain, position
 
 
 gain_matrix, let_to_ind, queries = setup()  # read input, set up cost_matrix and queries
