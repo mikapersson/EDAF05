5d9c4a21f97a280996f638818fba3a44f3ae9f93
mikapersson <perssonmika@gmail.com>
2020-05-09 16:43:20 +0200

    some comments

---
+++ Lab6-RailwayPlanning/read_data.py
@@ -0,0 +1,6 @@
+import sys
+
+
+def read_create():
+    N, M, C, P = list(map(int, sys.stdin.readline().replace('\n', '').split(' ')))
+
---
+++ Lab6-RailwayPlanning/solution.py
@@ -0,0 +1,10 @@
+from read_data import read_create
+
+# Create the graph
+
+# Have a method for finding the source (s) and sink node (t)?
+
+# Iterate FF alg. while removing some edges through P
+# and stop when the flow is below C.
+
+# Print the number of edges/routes we could remove + maximal flow
