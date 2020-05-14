b24c5a269553a370e2603a2e1a64a495020620f2
mikapersson <perssonmika@gmail.com>
2020-05-14 09:03:17 +0200

    start over

--- Lab6-RailwayPlanning/read_data.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-
-def read_create():
-    N, M, C, P = list(map(int, sys.stdin.readline().replace('\n', '').split(' ')))
-
--- Lab6-RailwayPlanning/solution.py
+++ Lab6-RailwayPlanning/solution.py
@@ -1,10 +1,26 @@
 from read_data import read_create
 
+""" Solution to lab 6 """
+
+N, M, C, P = list(map(int, sys.stdin.readline().replace('\n', '').split(' ')))  # read input
+
+
+
+
+
 # Create the graph
 
-# Have a method for finding the source (s) and sink node (t)?
+# Have a method for finding the source (s)? just get node with value 0?
 
 # Iterate FF alg. while removing some edges through P
 # and stop when the flow is below C.
 
 # Print the number of edges/routes we could remove + maximal flow
+
+
+
+'''
+FRÅGOR
+- plocka inte bort kant! lägg till kant istället, börja bakifrån!
+-> eftersom vi gör detta kommer s och t ändras? ska vi då ha koll på vad de aktuella s och t är?
+'''
