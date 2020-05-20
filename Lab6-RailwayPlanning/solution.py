978b1887e23665756d4924dafea363886dac1523
mikapersson <perssonmika@gmail.com>
2020-05-20 11:37:19 +0200

    2med.in now works

--- Lab6-RailwayPlanning/solution.py
+++ Lab6-RailwayPlanning/solution.py
@@ -7,19 +7,39 @@ from ford_fulkerson import ford_fulkerson
 sys.setrecursionlimit(10**9)
 # SETUP
 N, M, C, P = list(map(int, sys.stdin.readline().replace('\n', '').split(' ')))  # read input
-
 nodes, edges, P_edges = read_create(N, M, P)
 
 source = 0  # source node index
 sink = N - 1  # sink node index
 graph = {}  # dictionary representation of graph
-max_flow = 0
 
 
 # solution starts at row X
 
 # SOLUTION
-for edge in edges:  # create graph
+for index, edge in enumerate(edges):  # create graph
+    if index not in P_edges:
+        from_node = edge.destination1
+        from_node_index = from_node.index
+        to_node = edge.destination2
+        to_node_index = to_node.index
+
+        if from_node_index not in graph:
+            from_node.edges.append(edge)
+            graph[from_node_index] = from_node
+        else:
+            graph[from_node_index].edges.append(edge)
+
+        if to_node_index not in graph:
+            to_node.edges.append(edge)
+            graph[to_node_index] = to_node  # undirected graph
+        else:
+            graph[to_node_index].edges.append(edge)
+
+iteration = 0
+max_flow = ford_fulkerson(graph, source, sink)  # current max-flow in 'graph'
+while max_flow < C:                             # add an edge in 'P_edges' at a time
+    iteration += 1
     new_edge_index = P_edges.pop()  # O(1)
     new_edge = edges[new_edge_index]
 
@@ -40,15 +60,9 @@ for edge in edges:  # create graph
     else:
         graph[to_node_index].edges.append(new_edge)
 
-# remove one edge at a time starting from P[0]
-removed_edges = 0
-while max_flow < C:
-    """We construct the graph G 'backwards' with respect to P_list until we are above C capacity"""
-    removed_edges += 1
-
     max_flow = ford_fulkerson(graph, source, sink)  # find maximal flow with Ford-Fulkersons algorithm
-    # print("iteration", removed_edges)
-    # print(max_flow)
+    print("iteration", iteration)
+    print(max_flow)
 
 # print("iterations:", iterations)
 print(len(P_edges), max_flow)  # len(P_edges) contain how many routes we didn't have to use (#removed edges)
