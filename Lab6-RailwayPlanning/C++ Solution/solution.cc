af74cbe8192a9e815a69c9ef8d681c38844a4419
mikapersson <perssonmika@gmail.com>
2020-08-02 23:16:42 +0200

    represeting graph as a vector

--- Lab6-RailwayPlanning/C++ Solution/solution.cc
+++ Lab6-RailwayPlanning/C++ Solution/solution.cc
@@ -1,4 +1,11 @@
 #include "setup.cc"
+#include <algorithm>
+
+typedef vector<Node*> Graph;
+
+Graph create_graph(node_vector& nodes, edge_vector& edges, route_vector& routes);
+void free_nodes(node_vector& nodes);
+void free_edges(edge_vector& edges);
 
 int main(){
     int nr_nodes, nr_edges, capacity, nr_routes;
@@ -7,8 +14,34 @@ int main(){
     node_vector nodes;
     edge_vector edges;
     route_vector routes;
-
     std::tie(nodes, edges, routes) = setup(nr_nodes, nr_edges, nr_routes);
 
+    Graph graph = create_graph(nodes, edges, routes);  // initial graph without route edges
+
+
+    // Free dynamically allocated memory (raw pointers)
+    free_nodes(nodes);
+    free_edges(edges);
+
     return 0;
+}
+
+Graph create_graph(node_vector& nodes, edge_vector& edges, route_vector& routes){
+    Graph graph(nodes.size());  // elements default initialized to nullptr
+
+
+
+    return graph;
+}
+
+void free_nodes(node_vector& nodes){
+    for(auto& n : nodes){
+        delete n;
+    }
+}
+
+void free_edges(edge_vector& edges){
+    for(auto& e : edges){
+        delete e;
+    }
 }
