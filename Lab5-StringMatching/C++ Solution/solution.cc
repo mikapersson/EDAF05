c822e24b42683bd2f78c5c418caf71fc64b5d570
mikapersson <perssonmika@gmail.com>
2020-08-02 08:57:02 +0200

    started working on recursive function

--- Lab5-StringMatching/C++ Solution/solution.cc
+++ Lab5-StringMatching/C++ Solution/solution.cc
@@ -18,19 +18,41 @@ int main() {
     std::tie(gain_matrix, letter_to_index, queries) = setup();
     
 
+
 }
 
 // Define functions used in main
+/**
+ * Calculates the gain of the two letters 'c1' and 'c2'
+ */
 int gain(const char& c1, const char& c2){
-
+    return gain_matrix[letter_to_index[c1]][letter_to_index[c2]];
 }
 
+/**
+ * Find optimal alignment for all the string-pairs in the global vector 'queries',
+ * such that the total gain of aligning the strings is maximized
+ */
 void align_words(){
+    string aligned1;
+    string aligned2;
+    for(const auto& q : queries){
+        word1 = q.first;
+        word2 = q.second;
 
+        align_words_rec(word1.size()-1, word2.size()-1);
+        std::tie(aligned1, aligned2) = backtrack();
+        cout << aligned1 << " " << aligned2 << endl;
+        align_cache.clear();
+    }
 }
 
+/**
+ * Recursive help function for 'align_words', fills out 'align_cache'
+ */
 pair<int, int> align_words_rec(const int& pos1, const int& pos2){
-
+    // If we've already computed the value for 'pos1' and 'pos2'
+    
 }
 
 pair<string, string> backtrack(){
